293c238a6ab4768085faf7a5b399de38458dcc00
Anshika Maurya <anshika.maurya137@gmail.com>
2026-03-23 10:19:09 +0530

    Added homepage and templates configuration

--- edutest/urls.py
+++ edutest/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from exams import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('home/', views.home),
+    path('login/', views.login),
 ]
